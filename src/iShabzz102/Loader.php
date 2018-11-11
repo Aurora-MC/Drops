7312d2770a7458736bb34c7a826128981c1e0bdc
iShabzz102 <44269742+iShabzz102@users.noreply.github.com>
2018-11-11 10:49:38 +0000

    Create Loader.php

---
+++ src/iShabzz102/Loader.php
@@ -0,0 +1,17 @@
+<?php
+namespace iShabzz102;
+//PluginBase
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase{
+
+    public function onEnable() : void{
+        $this->getLogger()->info("Drops Disabled!");
+    }
+        public function onDeath(PlayerDeathEvent $event){
+     $player = $event->getPlayer();
+     $event->setDrops([]);
+        }
+}
