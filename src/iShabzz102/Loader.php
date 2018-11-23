803ab94472ddc46c49c94031c1feeff67fb9114a
iShabzz102 <44269742+iShabzz102@users.noreply.github.com>
2018-11-23 20:17:56 +0000

    Update Loader.php
    Update V1 - Added Config.

--- src/iShabzz102/Loader.php
+++ src/iShabzz102/Loader.php
@@ -3,16 +3,35 @@ namespace iShabzz102;
 //PluginBase
 use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+class Loader extends PluginBase implements Listener {
+        public $prefix = TF::GOLD."[".TF::GREEN."(!)".TF::AQUA."drops".TF::GOLD."] ".TF::RESET;
 
-class Loader extends PluginBase{
-
-    public function onEnable() : void{
-        $this->getLogger()->info("Drops Disabled!");
-    }
-        public function onDeath(PlayerDeathEvent $event){
-     $player = $event->getPlayer();
-     $event->setDrops([]);
-        }
+ public function onEnable(): void {
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  if($this->getConfig()->get("drops") !== true){
+      $statut = "disabled";
+  }else{
+      $statut = "enabled";
+  }
+  $this->getServer()->getLogger()->notice($this->prefix.TF::RED."You can edit it in config.yml");
+  $this->getServer()->getLogger()->notice($this->prefix.TF::GREEN."Drops $statut");
+ }
+   public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+       //Adding Commands Soon
+   }
 }
+ public function PlayerDeath(PlayerDeathEvent $ev){
+  if($this->getConfig()->get("drops") == false) {
+      $ev->setDrops([]);
+  }else{
+return;
+        }
+  }
+ }
