2405fa1cdac0fb69f37bbc6d9b4cdee34946d3c1
VixikHD <gamak.mcpe@gmail.com>
2018-11-24 20:31:28 +0100

    Small cleanup

--- src/iShabzz102/Loader.php
+++ src/iShabzz102/Loader.php
@@ -1,58 +1,71 @@
 <?php
+
+declare(strict_types=1);
+
 namespace iShabzz102;
-//PluginBase
-use pocketmine\Server;
-use pocketmine\Player;
+
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as TF;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 
+/**
+ * Class Loader
+ * @package iShabzz102
+ */
 class Loader extends PluginBase implements Listener {
-	public $prefix = TF::GOLD."[".TF::GREEN."(!)".TF::AQUA."drops".TF::GOLD."]".TF::RESET;
+    
+    /** @var string $prefix */
+    private $prefix = "§6[§a(!)§bdrops§6]§r";
 
-	public function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		if($this->getConfig()->get("drops") !== true){
-			$statut = "disabled";
-		}else{
-			$statut = "enabled";
-		}
-		$this->getServer()->getLogger()->notice($this->prefix.TF::RED." You can edit it in config.yml");
-		$this->getServer()->getLogger()->notice($this->prefix.TF::GREEN." Drops $statut");
-	}
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-		if (!empty($args)) {
-			switch ($args[0]) {
-				case 'on':
-					$this->getConfig()->set("drops", true);
-					$this->getConfig()->save();
-					$sender->sendMessage($this->prefix.TF::GREEN." Drops have been enabled");
-					break;
-				
-				case 'off':
-					$this->getConfig()->set("drops", false);
-					$this->getConfig()->save();
-					$sender->sendMessage($this->prefix.TF::GREEN." Drops have been disabled");
-					break;
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if ($this->getConfig()->get("drops") !== true) {
+            $statut = "disabled";
+        } else {
+            $statut = "enabled";
+        }
+        $this->getServer()->getLogger()->notice($this->prefix . "§c You can edit it in config.yml");
+        $this->getServer()->getLogger()->notice($this->prefix . "§a Drops $statut");
+    }
 
-				default:
-					$sender->sendMessage($this->prefix.TF::RED." Usage:".TF::GREEN." /drops <on:off>");
-					break;
-			}
-		} else {
-			$sender->sendMessage($this->prefix.TF::RED." Usage:".TF::GREEN." /drops <on:off>");
-		}
-		return true;
-	}
-	public function PlayerDeath(PlayerDeathEvent $ev){
-		if($this->getConfig()->get("drops") == false) {
-			$ev->setDrops([]);
-		}else{
-			return;
-		}
-	}
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param array $args
+     * 
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (isset($args)) {
+            switch ($args[0]) {
+                case 'on':
+                    $this->getConfig()->set("drops", true);
+                    $this->getConfig()->save();
+                    $sender->sendMessage($this->prefix . "§a Drops have been enabled");
+                    break;
+
+                case 'off':
+                    $this->getConfig()->set("drops", false);
+                    $this->getConfig()->save();
+                    $sender->sendMessage($this->prefix . "§a Drops have been disabled");
+                    break;
+                default:
+                    $sender->sendMessage($this->prefix . "§c Usage:§7 /drops <on:off>");
+                    break;
+            }
+        } else {
+            $sender->sendMessage($this->prefix . "§c Usage:§7 /drops <on:off>");
+        }
+        return true;
+    }
+
+    /**
+     * @param PlayerDeathEvent $ev
+     */
+    public function PlayerDeath(PlayerDeathEvent $ev) {
+        if ($this->getConfig()->get("drops") == false) $ev->setDrops([]);
+    }
 }
